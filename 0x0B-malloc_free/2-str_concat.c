477ae8143f2a2d807c885317245a04418b4291c2
GitLMB12 <mhamedbenzaquiii@gmail.com>
2023-11-04 09:16:01 -0700

    concat_use_malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* str_concat - a function that concatenates two strings
+*@s1:First string
+*@s2:Second string
+*
+*
+*Return: NULL in case of failure
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int x, y;
+
+	if (s1 == NULL)
+		return (NULL);
+	x = 0;
+	y = 0;
+	for (; s2[x]; x++)
+	{
+		if (x == 0)
+		{
+			for (; s1[x]; x++)
+				y++;
+		}
+		y++;
+	}
+	concat = malloc(sizeof(char) * (y + 1));
+	if (concat == NULL)
+		return (NULL);
+	x = 0;
+	y = 0;
+	while (s1[x] != '\0')
+	{
+		concat[y] = s1[x];
+		x++;
+		y++;
+	}
+	x = 0;
+	while (s2[x] != '\0')
+	{
+		concat[y] = s2[x];
+		x++;
+		y++;
+	}
+	concat[y] = '\0';
+	return (concat);
+}
