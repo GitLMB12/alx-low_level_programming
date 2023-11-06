da90f4877eb47ec356b5ee8df4df194ecf281a15
GitLMB12 <mhamedbenzaquiii@gmail.com>
2023-11-06 03:35:43 -0800

    init_

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* init_dog - inits a variable of type dog
+*@d: dog identification
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
