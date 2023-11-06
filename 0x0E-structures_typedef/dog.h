f0debbcdf4f2b96acc4c3d2df6e088ef3d1c08dd
GitLMB12 <mhamedbenzaquiii@gmail.com>
2023-11-06 03:16:10 -0800

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,17 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-/*
- * typedef - rename the struct
- * my_dog = the name of struct
+/**
+*struct dog - a new type of data
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
 */
-
 typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-} my_dog;
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
