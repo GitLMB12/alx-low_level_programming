c94cd4dfd48f1e183debc7a7140ab82e96ad3a08
GitLMB12 <mhamedbenzaquiii@gmail.com>
2023-12-13 22:01:47 -0800

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,15 +10,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal_value = 0;
-	int i = 0;
-
+	int i;
+	
+	i = 0 ;
 	if (!b)
 		return (0);
-	while (*b)
+	while (b[i])
 	{
 		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
-	decimal_value = 2 * decimal_value + (b[i] + '0');
+	decimal_value = 2 * decimal_value + (b[i] - '0');
+	i++;
 	}
 	return (decimal_value);
 }
